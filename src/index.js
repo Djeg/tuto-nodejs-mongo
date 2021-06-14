4d049d72252d3857db2626d4c1c409336b3ad191
David Jegat <david.jegat@gmail.com>
2021-06-14 16:14:17 +0200

    Tuto promesses

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-console.log('coucou')
+// Require la librairie fastify
+const fastify = require('fastify')
 
-console.log('test')
+// On créé une application
+const app = fastify({ logger: true })
+
+// On lui attache « la route » (le path, l'URI, chemin)  suivi
+// d'une callback en utilisant la fonction « get »
+app.get('/', () => {
+  return 'Hello World'
+})
+
+app.get('/hello', () => {
+  return 'Hello World !!!!!'
+})
+
+const start = async () => {
+  await app.listen(4545)
+}
+
+start()
