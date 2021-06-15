2865aeb2387d81f2ab470788bea90ce1f15092b8
David Jegat <david.jegat@gmail.com>
2021-06-15 12:28:08 +0200

    Ajout de mongodb

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 // Require la librairie fastify
 const fastify = require('fastify')
+// On inclue la librairie mongo
+const mongo = require('mongodb')
 
 // On créé une application
 const app = fastify({ logger: true })
 
 // On lui attache « la route » (le path, l'URI, chemin)  suivi
 // d'une callback en utilisant la fonction « get »
+
 app.get('/', () => {
   return 'Hello World'
 })
@@ -23,8 +26,17 @@ app.post('/categories', (request, reply) => {
   return { status: 200 }
 })
 
-app.get('/articles', () => {
-  return [{ title: 'Mon premier article' }]
+app.get('/articles', async (request) => {
+  // Grâce à la décoration, nous pouvons accéder
+  // à la base de données en utilisant :
+  // this.db
+  const db = request.db
+
+  // Nous récupérons la totalité des articles, que
+  // nous formattons en un tableaux javascript
+  const articles = await db.collection('articles').find().toArray()
+
+  return articles
 })
 
 app.post('/articles', (request) => {
@@ -34,6 +46,20 @@ app.post('/articles', (request) => {
 })
 
 const start = async () => {
+  // on se connecte à la base de données
+  const db = await mongo.MongoClient.connect(
+    'mongodb+srv://test:test@cluster0.suzgw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  )
+
+  // Nous obtenons la base de donnnées blog
+  const blogBDD = db.db('blog')
+
+  // Nous décorons fastify en ajoutant
+  // le mot clefs "db"  à la request de
+  // toutes mes routes
+  app.decorateRequest('db', blogBDD)
+
+  // On démarre l'application
   await app.listen(4545)
 }
 
