55fcf472f4c7d0ba7f0886ed6245f9736bb99f35
David Jegat <david.jegat@gmail.com>
2021-06-17 12:41:15 +0200

    Replace space for windows

--- bin/copy-past-env.js
+++ bin/copy-past-env.js
@@ -7,8 +7,8 @@ let envPath = path.join(__dirname, '..', '.env')
 let envDistPath = path.join(__dirname, '..', '.env.dist')
 
 if (/win/.test(process.platform)) {
-  envDistPath = envDistPath.slice(1)
-  envPath = envPath.slice(1)
+  envDistPath = envDistPath.slice(1).replace(/%20/g, ' ')
+  envPath = envPath.slice(1).replace(/%20/g, ' ')
 }
 
 // Si le fichier `.env` n'éxiste pas
