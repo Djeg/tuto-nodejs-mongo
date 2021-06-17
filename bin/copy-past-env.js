5e459d98872a7f9bdae0bfc5d85b382aa155dc3c
David Jegat <david.jegat@gmail.com>
2021-06-17 12:16:55 +0200

    Use path.join

--- bin/copy-past-env.js
+++ bin/copy-past-env.js
@@ -3,9 +3,11 @@ import fs from 'fs'
 import path from 'path'
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname)
+const envPath = path.join(__dirname, '..', '.env')
+const envDistPath = path.join(__dirname, '..', '.env.dist')
 
 // Si le fichier `.env` n'éxiste pas
-if (!fs.existsSync(`${__dirname}/../.env`)) {
+if (!fs.existsSync(envPath)) {
   // Nous copions/collons le fichier `.env.dist` dans `.env`
-  fs.copyFileSync(`${__dirname}/../.env.dist`, `${__dirname}/../.env`)
+  fs.copyFileSync(envDistPath, envPath)
 }
