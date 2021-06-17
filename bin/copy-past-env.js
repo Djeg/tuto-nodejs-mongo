babee711e24dedcf562a57acf70b221def6ad448
David Jegat <david.jegat@gmail.com>
2021-06-17 12:32:04 +0200

    Add os specific path checking

--- bin/copy-past-env.js
+++ bin/copy-past-env.js
@@ -6,6 +6,11 @@ const __dirname = path.dirname(new URL(import.meta.url).pathname)
 const envPath = path.join(__dirname, '..', '.env')
 const envDistPath = path.join(__dirname, '..', '.env.dist')
 
+if (/win/.test(process.platform)) {
+  envDistPath = envDistPath.slice(1)
+  envPath = envPath.slice(1)
+}
+
 // Si le fichier `.env` n'éxiste pas
 if (!fs.existsSync(envPath)) {
   // Nous copions/collons le fichier `.env.dist` dans `.env`
