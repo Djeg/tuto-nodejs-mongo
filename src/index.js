a9985d694d437a694ce4394094baac853f047440
David Jegat <david.jegat@gmail.com>
2021-08-31 11:05:49 +0200

    Installation de fastify

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Bienvenue')
+// On importe la librairie fastify
+const fastify = require('fastify')
+
+// Créer une application fastify.
+// Une application fastify c'est ce qui vas contenir
+// toutes nos routes
+const app = fastify({ logger: true })
+
+// On lance le serveur sur le port 8080
+app.listen(8080)
