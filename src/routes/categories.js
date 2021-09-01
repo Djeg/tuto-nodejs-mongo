cabd35dd32e76bd267251cbf1984ca5e93549adb
David Jegat <david.jegat@gmail.com>
2021-09-01 16:40:24 +0200

    Préparation de l'exo

---
+++ src/routes/categories.js
@@ -0,0 +1,7 @@
+/**
+ * 1. Créer un plugin fastify dans ce fichier
+ * 2. Brancher le plugin dans le fichier
+ *    src/index.js
+ * 3. Couper / Coller et retoucher le code
+ *    des routes: get /categories et post /categories
+ */
