cf14e6804a67626987256b848252e574ce05920f
David Jegat <david.jegat@gmail.com>
2021-09-20 15:27:56 +0200

    Ajout d'un server http simple

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log('Mon application')
+// Ancienne syntax pour importer une librairie js :
+// const fastify = require('fastify')
+import fastify from 'fastify'
+
+async function start() {
+  /**
+   * Création d'une application fastify qui nous
+   * permet de démarrer un (logical) server http
+   */
+  const app = fastify({
+    // Cette option permet d'afficher dans la console
+    // ce qui se produit sur le server. Comme par
+    // éxemple un erreur.
+    logger: true,
+  })
+
+  console.log('Démarrage du server sur le port 9090')
+
+  /**
+   * On démarre le server sur le port 9090. app.listen
+   * démarre un programme qui ne finie jamais et écoute
+   * les requêtes sur le port 9090
+   */
+  await app.listen(9090)
+}
+
+start()
