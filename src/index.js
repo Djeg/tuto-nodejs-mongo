5ed8aef7c740a90c8bdd7300d556f61d34384b5b
David Jegat <david.jegat@gmail.com>
2021-09-20 16:02:36 +0200

    Ajout de 2 routes get simple

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 // Ancienne syntax pour importer une librairie js :
 // const fastify = require('fastify')
+
+/**
+ * On importe la librairie fastify
+ */
 import fastify from 'fastify'
 
+/**
+ * On créé une fonction de démarrage asynchrone afin
+ * de pouvoir utiliser le mot clefs await
+ */
 async function start() {
   /**
    * Création d'une application fastify qui nous
@@ -10,12 +18,29 @@ async function start() {
   const app = fastify({
     // Cette option permet d'afficher dans la console
     // ce qui se produit sur le server. Comme par
-    // éxemple un erreur.
+    // éxemple une erreur.
     logger: true,
   })
 
-  console.log('Démarrage du server sur le port 9090')
+  /**
+   * On route de test, pour dire bonjour
+   */
+  app.get('/hello', async () => {
+    return 'Hello tout le monde'
+  })
 
+  /**
+   * On route de test, pour dire bonjour
+   */
+  app.get('/user', async () => {
+    return {
+      id: 1,
+      username: 'john',
+      email: 'john@mail.com',
+    }
+  })
+
+  console.log('Démarrage du server sur le port 9090')
   /**
    * On démarre le server sur le port 9090. app.listen
    * démarre un programme qui ne finie jamais et écoute
@@ -24,4 +49,7 @@ async function start() {
   await app.listen(9090)
 }
 
+/**
+ * On lance la fonction de démarrage
+ */
 start()
