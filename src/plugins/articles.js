d8c15eb41aca96b3b2942298ff51d8ed7fc12387
David Jegat <david.jegat@gmail.com>
2021-09-23 11:22:41 +0200

    Correction exo 5 - 6

--- src/plugins/articles.js
+++ src/plugins/articles.js
@@ -76,6 +76,76 @@ export default async function articlesPlugin(app) {
       return article
     }
   )
+
+  /**
+   * Mise à jour d'un article
+   */
+  app.patch(
+    '/articles/:id',
+    {
+      schema: {
+        body: UpdateArticleSchema,
+        response: {
+          200: ArticleSchema,
+        },
+      },
+    },
+    async (request, reply) => {
+      try {
+        const result = await app.db
+          .collection('articles')
+          .updateOne(
+            { _id: mongo.ObjectId(request.params.id) },
+            { $set: request.body }
+          )
+
+        if (0 === result.modifiedCount) throw Error()
+
+        const article = await app.db.collection('articles').findOne({
+          _id: mongo.ObjectId(request.params.id),
+        })
+
+        return article
+      } catch (e) {
+        reply.status(404)
+
+        return { message: 'Article not found' }
+      }
+    }
+  )
+
+  /**
+   * Suppression d'un article
+   */
+  app.delete(
+    '/articles/:id',
+    {
+      schema: {
+        response: {
+          200: ArticleSchema,
+        },
+      },
+    },
+    async (request, reply) => {
+      try {
+        const article = await app.db.collection('articles').findOne({
+          _id: mongo.ObjectId(request.params.id),
+        })
+
+        if (!article) throw Error()
+
+        await app.db.collection('articles').deleteOne({
+          _id: mongo.ObjectId(request.params.id),
+        })
+
+        return article
+      } catch (e) {
+        reply.status(404)
+
+        return { message: 'Article not found' }
+      }
+    }
+  )
 }
 
 /**
@@ -85,6 +155,15 @@ export const NewArticleSchema = S.object()
   .prop('title', S.string().required())
   .prop('description', S.string().required())
   .prop('content', S.string().required())
+  .additionalProperties(false)
+
+/**
+ * Schéma d'un article à mettre à jour
+ */
+export const UpdateArticleSchema = S.object()
+  .prop('title', S.string())
+  .prop('description', S.string())
+  .prop('content', S.string())
   .additionalProperties(false)
 
 /**
