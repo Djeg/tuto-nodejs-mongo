bbcc924cd127f724fe3c1bc510b5c8913da8103b
David Jegat <david.jegat@gmail.com>
2021-11-29 11:57:00 +0100

    Ajout d'un example de base

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('bienvenue')
+/**
+ * Nous importons la librairie fastify
+ */
+import Fastify from 'fastify'
+
+/**
+ * Nous pouvons créer une application fastify
+ */
+const app = Fastify({
+  /* Ici nous spécifions des options.
+  l'option logger nous permet de débugger notre
+  serveur lorsque nous avons des erreurs */
+  logger: true,
+})
+
+/**
+ * Nous pouvons démarer un server logique sur
+ * notre machine
+ */
+app.listen(3030, 'localhost', () => {
+  console.log("Le server est disponible sur l'adresse : http://localhost:3030")
+})
